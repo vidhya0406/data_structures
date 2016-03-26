878a254764395cb62dc7bd2420f978425c5a0085
Vidhya Ranganathan <vidhya0406@gmail.com>
2016-03-26 04:41:43 -0400

    binary search

---
+++ binary_search_recur.py
@@ -0,0 +1,15 @@
+def binarySearch(alist, item):
+    if len(alist) == 0:
+        return False
+    else:
+        midpoint = len(alist)//2
+        if alist[midpoint]==item:
+            return True
+        else:
+            if item<alist[midpoint]:
+                return binarySearch(alist[:midpoint],item)
+            else:
+                return binarySearch(alist[midpoint+1:],item)
+list_to_search = input('Search data: ')
+find_me = input('Find me: ')
+print(str(binarySearch(list_to_search, find_me)))
